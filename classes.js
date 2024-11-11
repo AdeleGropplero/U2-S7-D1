68fe996d6b612d5159b71b7bedd54e2163744ac8
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-11 16:05:34 +0100

    patial ex.2

--- classes.js
+++ classes.js
@@ -29,3 +29,48 @@ console.log(mamma);
 
 adele.compareAge(chiara);
 adele.compareAge(mamma);
+
+/* Esercizio 2 */
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+}
+
+const form = document.getElementById("petForm");
+
+/* const pets = [] */
+
+let petNames = [];
+let ownerNames = [];
+let specieses = [];
+let breeds = [];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const myArrays = function () {
+    const petName = document.getElementById("petName").value;
+    const ownerName = document.getElementById("ownerName").value;
+    const species = document.getElementById("species").value;
+    const breed = document.getElementById("breed").value;
+
+    petNames.push(petName);
+    ownerNames.push(ownerName);
+    specieses.push(species);
+    breeds.push(breed);
+  };
+  myArrays();
+  alert("Dati salvati temporaneamente");
+
+  form.reset();
+
+  console.log(petNames);
+  console.log(ownerNames);
+  console.log(specieses);
+  console.log(breeds);
+});
---
+++ script.js
@@ -0,0 +1,32 @@
+/* const form = document.getElementById("petForm");
+
+let petNames = [];
+let ownerNames = [];
+let specieses = [];
+let breeds = [];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const myArrays = function () {
+    const petName = document.getElementById("petName").value;
+    const ownerName = document.getElementById("ownerName").value;
+    const species = document.getElementById("species").value;
+    const breed = document.getElementById("breed").value;
+
+    petNames.push(petName);
+    ownerNames.push(ownerName);
+    specieses.push(species);
+    breeds.push(breed);
+  };
+  myArrays();
+  alert("Dati salvati temporaneamente");
+
+  form.reset();
+
+  console.log(petNames);
+  console.log(ownerNames);
+  console.log(specieses);
+  console.log(breeds);
+});
+ */
